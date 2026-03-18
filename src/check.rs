95a9fdd78a38fae4f83cc09c4341aa5c1bab5548
Aneesh Abbineni <aabbineni@all.health>
2026-03-17 22:16:59 -0400

    Added Serde

--- src/check.rs
+++
@@ -1,9 +0,0 @@
-pub fn check_valid(map: &Vec<&'static str>) {
-    let mut max_size: usize = 1;
-    
-    for value in map {
-       if !value.is_empty() {
-           max_size = max_size.max(value.len());
-       } 
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-mod check;
-use check::check_valid;
-
 fn main() {
     println!("Hello, world!");
-
-    let codes = vec![
-        "b", "c", "by", "i", 
-        "l", "q", "ub", "ui", 
-        "ul", "uq", "f", "d", 
-        "s", "byt", "a", "dy",
-        "ad", "p", "rc", "w",
-        "un", "r", "re", "t",
-        "te", "o", "v", "fu"
-    ];
-
-    check_valid(&codes);
 }
