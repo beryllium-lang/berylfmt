7702e684d3b6334a6937599829f8639cd7f6d165
Aneesh Abbineni <aabbineni@all.health>
2026-03-18 21:30:15 -0400

    Added a struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+struct FmtConfig {
+    tab_size: u8,
+    column_limit: u8,
+    declaration_spacing: u8
+}
+
 fn main() {
     println!("Hello, world!");
 }
